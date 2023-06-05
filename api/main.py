c1879693e5a91a2874ab37de958e19d2530b6e16
e-l-w-s <dingler.stefan@gmail.com>
2023-06-05 14:05:26 +0200

    Test with OS

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 # import spotipy
-# import os
+import os
 
 # from google.oauth2 import service_account
 # from apiclient import discovery
@@ -63,18 +63,19 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        # service_account_credentials = {
-        #     "type": os.environ.get("TYPE"),
-        #     "project_id": os.environ.get("PROJECT_ID"),
-        #     "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
-        #     "private_key": os.environ.get("PRIVATE_KEY"),
-        #     "client_email": os.environ.get("CLIENT_EMAIL"),
-        #     "client_id": os.environ.get("CLIENT_ID"),
-        #     "auth_uri": os.environ.get("AUTH_URI"),
-        #     "token_uri": os.environ.get("TOKEN_URI"),
-        #     "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER"),
-        #     "client_x509_cert_url": os.environ.get("CLIENT_URL")
-        # }
+        service_account_credentials = {
+            "type": os.environ.get("TYPE"),
+            "project_id": os.environ.get("PROJECT_ID"),
+            "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+            "private_key": os.environ.get("PRIVATE_KEY"),
+            "client_email": os.environ.get("CLIENT_EMAIL"),
+            "client_id": os.environ.get("CLIENT_ID"),
+            "auth_uri": os.environ.get("AUTH_URI"),
+            "token_uri": os.environ.get("TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER"),
+            "client_x509_cert_url": os.environ.get("CLIENT_URL")
+        }
+        print(service_account_credentials)
         # credentials = service_account.Credentials.from_service_account_info(
         #     service_account_credentials, scopes=SCOPES)
         # service = discovery.build('sheets', 'v4', credentials=credentials)
