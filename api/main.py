dafddb2d7ab129212249015536a597d04b4f716c
e-l-w-s <dingler.stefan@gmail.com>
2023-06-05 15:02:50 +0200

    New try

--- api/main.py
+++ api/main.py
@@ -88,7 +88,7 @@ class handler(BaseHTTPRequestHandler):
         service = discovery.build('sheets', 'v4', credentials=credentials)
 
         artists = getFollowedArtists()
-        print(artists)
+
         update = []
         for idx, artist in enumerate(artists):
             albums = getArtistAlbums(artists[artist])
@@ -96,7 +96,6 @@ class handler(BaseHTTPRequestHandler):
                 print("=====================\nNew albums for", artist, artists[artist])
                 for idx, album in enumerate(albums):
                     update.append([artist,album,albums[album]])
-            break
 
         dict_me = dict(values=update)
 
