303ba273582f7db8272cd9e94fdb7ecacecd155f
e-l-w-s <dingler.stefan@gmail.com>
2023-06-06 12:43:28 +0200

    Removed spotipy and created access token function

--- api/main.py
+++ api/main.py
@@ -1,13 +1,10 @@
-from http.server import BaseHTTPRequestHandler
-import spotipy
+import logging
+import requests
 import os
 
-# https://stackoverflow.com/questions/75869291/cache-errors-using-spotify-api-in-python-program
-cache_dir = '.cache'
-if not os.path.exists(cache_dir):
-    os.makedirs(cache_dir)
-
-#os.chmod(cache_dir, 0o700)
+from flask import Flask, request, redirect
+from dotenv import load_dotenv
+from base64 import b64encode
 
 from google.oauth2 import service_account
 from apiclient import discovery
@@ -15,100 +12,153 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SAMPLE_SPREADSHEET_ID = '1JOSHfmHUWOorpyoWBn0m0Zacrd3xxy5XZdFZFSUJVvs'
 SAMPLE_RANGE_NAME = 'A:C'
 
-
 from datetime import datetime, timedelta
 current_date = datetime.now()
-check_date = current_date - timedelta(days=7)
+check_date = current_date - timedelta(days=7000)
 
-from spotipy.oauth2 import SpotifyOAuth
-print("CUSTOMER_ID", os.environ.get("CUSTOMER_ID"))
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=os.environ.get("CUSTOMER_ID"), client_secret=os.environ.get("SECRET_ID"),redirect_uri="http://localhost/",scope="user-follow-read,user-read-recently-played", cache_path='cache.txt'))
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)  # Set the desired logging level
 
-def getArtistAlbums(uuid):
+app = Flask(__name__)
+load_dotenv()
+
+@app.route("/")
+def index():
+    client_id = os.getenv("CLIENT_ID")
+    redirect_uri = os.getenv("REDIRECT_URI")
+    scope = "user-follow-read"  # Add any additional scopes you need
+
+    authorize_url = f"https://accounts.spotify.com/authorize?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope={scope}"
+    return redirect(authorize_url)
+
+@app.route("/callback")
+def callback():
+    code = request.args.get("code")
+    if code:
+        access_token = get_access_token(code)
+        # Use the access token to make requests to the Spotify API
+        # Implement your logic here
+        artists = getFollowedArtists(access_token)
+        
+        update = []
+        for artist in artists:
+            name = artist["name"].title()
+            id = artist["id"]
+            app.logger.error(f"Get albums for: {name} / {id}")
+            albums = getArtistAlbums(access_token, id)
+
+            if albums:
+                print("=====================\nFound new albums for", name)
+                for idx, album in enumerate(albums):
+                    update.append([name,album["name"],album["release_date"]])
+            break
+        
+        updateGoogleSheet(update)
+
+        #return "Authorization successful! You can close this page."
+        return update
+    else:
+        error = request.args.get("error")
+        # Handle the error case appropriately
+        app.logger.error(f"Authorization failed. Error: {request}")
+        return f"Authorization failed: {error}"
+
+def updateGoogleSheet(data):
+    dict_me = dict(values=data)
+
+    # service_account_credentials = {
+    #     "type": os.environ.get("type"),
+    #     "project_id": os.environ.get("project_id"),
+    #     "private_key_id": os.environ.get("private_key_id"),
+    #     "private_key": os.environ.get("private_key"),
+    #     "client_email": os.environ.get("client_email"),
+    #     "client_id": os.environ.get("client_id"),
+    #     "auth_uri": os.environ.get("auth_uri"),
+    #     "token_uri": os.environ.get("token_uri"),
+    #     "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
+    #     "client_x509_cert_url": os.environ.get("client_x509_cert_url")
+    # }
+
+    # print(service_account_credentials["private_key"])
+    # credentials = service_account.Credentials.from_service_account_info(
+    #     service_account_credentials, scopes=SCOPES)
+    
+    credentials = service_account.Credentials.from_service_account_file(
+        "spotify-new-releases-388907-3a50d5a91da5.json", scopes=SCOPES)
+
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+
+    service.spreadsheets().values().append(
+        spreadsheetId=SAMPLE_SPREADSHEET_ID,
+        valueInputOption='RAW',
+        range=SAMPLE_RANGE_NAME,
+        body=dict_me).execute()
+
+    print('Sheet successfully Updated')
+    return    
+
+def getFollowedArtists(access_token):
+
+    headers = { "Authorization": "Bearer {}".format(access_token)}
+    response = requests.get("https://api.spotify.com/v1/me/following?type=artist&limit=50", headers=headers)
+    result = response.json()['artists']
+
+    artists = []
+    artists.extend(result['items'])
+    while result['next']:
+        after = result['next'].split("&after=")[1]
+        response = requests.get(f"https://api.spotify.com/v1/me/following?type=artist&limit=50&after={after}", headers=headers)
+        result = response.json()['artists']
+        artists.extend(result['items'])
+        break
+
+    return artists
+
+def getArtistAlbums(access_token, id):
+    headers = { "Authorization": "Bearer {}".format(access_token)}
+    response = requests.get(f"https://api.spotify.com/v1/artists/{id}/albums?include_groups=album&market=DE&limit=50", headers=headers)
+    result = response.json()
+
     albums = []
-    results = sp.artist_albums(uuid, album_type='album')
-    albums.extend(results['items'])
-    while results['next']:
-        results = sp.next(results)
-        albums.extend(results['items'])
-
-    unique = {}
-    for album in albums:
-        name = album['name'].title()
-
+    for album in result["items"]:
         if album["release_date_precision"] == "day":
             release_date = datetime.strptime(album['release_date'], "%Y-%m-%d")
         else:
             continue
 
-        if ( name not in unique ) & (release_date >= check_date):
-            unique[name] = album['release_date']
-    return unique
+        if release_date >= check_date:
+            app.logger.error("Found new album")
+            albums.append(album)
 
+    return albums
+    
+def getAuth():
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    return b64encode(f"{client_id}:{client_secret}".encode()).decode(
+        "ascii"
+    )
 
-def getFollowedArtists():
-    artists = []
-    results = sp.current_user_followed_artists()
-    artists.extend(results['artists']['items'])
-    while results['artists']['next']:
-        after = results['artists']['next'].split("&after=")[1]
-        results = sp.current_user_followed_artists(after=after)
-        artists.extend(results['artists']['items'])
-    print(f"Added {len(artists)} artists")
+def get_access_token(code):
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+    redirect_uri = os.getenv("REDIRECT_URI")
 
-    unique = {}
-    for artist in artists:
-        name = artist['name'].title()
-        if name not in unique:
-            unique[name] = artist["id"]
-        # break
-    return unique
+    app.logger.error(client_id, client_secret, redirect_uri)
+    headers = {"Authorization": "Basic {}".format(getAuth())}
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        s = self.path
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
+    token_url = "https://accounts.spotify.com/api/token"
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": redirect_uri,
+        "client_id": client_id,
+        "client_secret": client_secret,
+    }
+    app.logger.error(token_url, data)
+    response = requests.post(token_url, data=data, headers=headers)
+    response_data = response.json()
+    return response_data["access_token"]
 
-        artists = getFollowedArtists()
-
-        update = []
-        for idx, artist in enumerate(artists):
-            albums = getArtistAlbums(artists[artist])
-            if albums:
-                print("=====================\nNew albums for", artist, artists[artist])
-                for idx, album in enumerate(albums):
-                    update.append([artist,album,albums[album]])
-
-        dict_me = dict(values=update)
-
-        service_account_credentials = {
-            "type": os.environ.get("type"),
-            "project_id": os.environ.get("project_id"),
-            "private_key_id": os.environ.get("private_key_id"),
-            "private_key": os.environ.get("private_key"),
-            "client_email": os.environ.get("client_email"),
-            "client_id": os.environ.get("client_id"),
-            "auth_uri": os.environ.get("auth_uri"),
-            "token_uri": os.environ.get("token_uri"),
-            "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
-            "client_x509_cert_url": os.environ.get("client_x509_cert_url")
-        }
-        print(service_account_credentials["private_key"])
-        credentials = service_account.Credentials.from_service_account_info(
-            service_account_credentials, scopes=SCOPES)
-        service = discovery.build('sheets', 'v4', credentials=credentials)
-
-        service.spreadsheets().values().append(
-            spreadsheetId=SAMPLE_SPREADSHEET_ID,
-            valueInputOption='RAW',
-            range=SAMPLE_RANGE_NAME,
-            body=dict_me).execute()
-
-        print('Sheet successfully Updated')
-        return
-
-
-
-
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
