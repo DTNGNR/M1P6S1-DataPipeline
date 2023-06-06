98b8f052bbcf5921dbb2d84a2655c29ed307428b
e-l-w-s <dingler.stefan@gmail.com>
2023-06-06 12:47:31 +0200

    added BaseHTTPRequestHandler

--- api/main.py
+++ api/main.py
@@ -160,5 +160,14 @@ def get_access_token(code):
     response_data = response.json()
     return response_data["access_token"]
 
-if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        app.run(port=8000, debug=True)
+        
+        print('Sheet successfully Updated')
+        return
